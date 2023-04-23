7a341a3829ba8d2f9634347ab5a13a315a463730
leoxsousa2 <leoxsousa2@gmail.com>
2023-04-23 15:49:01 -0300

    add RegistradorTarefa.py e update info readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# RegistradorTarefas
-Este programa registra as tarefas informadas com data e hora e depois grava em um arquivo txt. Quando for registrar uma nova tarefa, o programa verifica o arquivo e informa as tarefas que já foram registradas para depois perguntar qual será a nova tarefa.
+# RegistradorDeTarefas
+Este programa registra as tarefas informadas com data e hora e depois grava em um arquivo txt. Quando por registrar uma nova tarefa, o programa verifica o arquivo e informa as tarefas que já foram registradas para depois perguntar qual será a nova tarefa.
+
+# Exemplo do funcionameto do programa (python):
+
+Digite s para sair
+
+* ID      Data    Hora    Tarefa
+* 1       2023-04-22      16:05:59        teste1
+* 2       2023-04-22      16:06:07        teste2
+* 3       2023-04-22      16:06:15        teste3
+
+Qual tarefa você vai realizar?
---
+++ RegistradorTarefas.py
@@ -0,0 +1,50 @@
+import os
+import datetime
+import time
+
+def tarefasExecutar():
+
+    # Limpa o terminal 
+    os.system('cls' if os.name == 'nt' else 'clear')  # limpa a tela do terminal
+    print("Digite s para sair")
+    print("     ")
+    
+    # lê o arquivo de tarefas se existir e mostra no terminal
+    data_atual = datetime.datetime.now().strftime("%Y-%m-%d")
+    nome_arquivo = f"tarefas do dia {data_atual}.txt"
+    if os.path.exists(nome_arquivo):
+        with open(nome_arquivo, "r") as arquivo:
+            conteudo = arquivo.read()
+        print(conteudo)
+
+    # pede para o usuário informar a tarefa
+    tarefa = input("Qual tarefa você vai realizar? ")
+
+    # verifica se o usuário informou "s" e encerra o programa se for o caso
+    if tarefa == "s":
+        print("Programa encerrado.")
+        time.sleep(2)
+        exit()
+
+    # gera o nome do arquivo com a data atual
+    data_atual = datetime.datetime.now().strftime("%Y-%m-%d")
+    nome_arquivo = f"tarefas do dia {data_atual}.txt"
+
+    # verifica se o arquivo já existe, se não existir, cria um novo
+    if not os.path.exists(nome_arquivo):
+        with open(nome_arquivo, "w") as arquivo:
+            arquivo.write("ID\tData\tHora\tTarefa\n")
+
+    # adiciona a nova tarefa ao final do arquivo
+    id_tarefa = sum(1 for _ in open(nome_arquivo))  # conta o número de linhas para gerar o ID da nova tarefa
+    hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+    with open(nome_arquivo, "a") as arquivo:
+        arquivo.write(f"{id_tarefa}\t{data_atual}\t{hora_atual}\t{tarefa}\n")
+
+    print(f"Tarefa registrada com sucesso no arquivo {nome_arquivo}!")
+    time.sleep(5)
+
+    tarefasExecutar() # Inicia a função tarefasExecutar  # indica que o programa deve continuar em loop
+
+
+tarefasExecutar() # Inicia a função tarefasExecutar
