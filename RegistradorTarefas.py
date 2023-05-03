fdcf453487fe06162f29ee9b7473f00006bd38f4
leoxsousa2 <leoxsousa2@gmail.com>
2023-05-03 12:00:42 -0300

    Otimização do código

--- RegistradorTarefas.py
+++ RegistradorTarefas.py
@@ -4,29 +4,26 @@ import time
 import requests
 import webbrowser
 import openai
-with open(r'C:\Users\Acer\OneDrive\Documentos\CredenciaisAPI\credenciaisAPI.txt') as f:
-    conteudo = f.readlines()
-    openai.api_key = conteudo[2].split(';')[1].strip() #Ler terceira linha coluna 2 (separacao ";" )
-#"C:\Users\Acer\OneDrive\Documentos\CredenciaisAPI\credenciaisAPI.txt"
+
+caminhoCredenciais= r'C:\Users\Acer\OneDrive\Documentos\CredenciaisAPI\credenciaisAPI.txt'
+caminhoArquivoSaida= r"C:\Users\Acer\OneDrive\Documentos\tarefas" #O resto sera preechido por strings
 
 def perguntarTarefa():
-    global tela   #pega a variavel que esta no inicio do codigo
+    global tela                                                         #pega a variavel que esta no inicio do codigo
     global tarefa
-    os.system('cls' if os.name == 'nt' else 'clear')  # limpa a tela do terminal
-
+    os.system('cls' if os.name == 'nt' else 'clear')                    # limpa a tela do terminal
     if tela == "Cheia" or tela == "Discreto":
-        os.system('cls' if os.name == 'nt' else 'clear')  # limpa a tela do terminal
+        os.system('cls' if os.name == 'nt' else 'clear')                # limpa a tela do terminal
         print("     ")
         print("Digite s=sair//d=modo-discreto//r=reiniciar//dark=modoescuro//edit=editar-arquivoTXT//abriP=abrir-pasta-arquivo//utxt=atualizar-arquivoTXT//abrirPtese//abrirPsimu")
         print("     ")
     if tela == "Cheia":
-        # lê o arquivo de tarefas se existir e mostra no terminal
         if os.path.exists(stringNomeArquivo()):
             with open(stringNomeArquivo(), "r") as arquivo:
                 conteudo = arquivo.read()
             print(conteudo)
             print("     ")
-        tarefa = input("Qual tarefa voce vai realizar? ")  # pede para o usuario informar a tarefa
+        tarefa = input("Qual tarefa voce vai realizar? ")               # pede para o usuario informar a tarefa
     if tela == "Discretro":
         print(previsaoTempo())
         print("     ")
@@ -34,22 +31,18 @@ def perguntarTarefa():
         print("     ")
         time.sleep(2)
         print("     ")
-        tarefa = input("Qual tarefa voce vai realizar? ")  # pede para o usuario informar a tarefa
+        tarefa = input("Qual tarefa voce vai realizar? ")               # pede para o usuario informar a tarefa
     if tela == "Dark":
-        os.system('cls' if os.name == 'nt' else 'clear')  # limpa a tela do terminal
+        os.system('cls' if os.name == 'nt' else 'clear')                # limpa a tela do terminal
         print("     ")
-        tarefa = input()  # pede para o usuario informar a tarefa
-
-
+        tarefa = input()                                                # pede para o usuario informar a tarefa
     if tarefa == "r":
         reniciar()
-    # verifica se o usuario informou "s" e encerra o programa se for o caso
     if tarefa == "s":
         print("Programa encerrado.")
         time.sleep(2)
-        os.system('cls' if os.name == 'nt' else 'clear')  # limpa a tela do terminal
+        os.system('cls' if os.name == 'nt' else 'clear')                # limpa a tela do terminal
         exit()
-    # verifica se o usuario informou "d" o programa para de mostrar informacoes do arquivo
     if tarefa == "d":
         tela= "Discretro"
         perguntarTarefa()
@@ -75,7 +68,7 @@ def perguntarTarefa():
         os.startfile(folder_path)
         reniciar()
     if tarefa == "utxt":
-        os.system('cls' if os.name == 'nt' else 'clear')  # limpa a tela do terminal
+        os.system('cls' if os.name == 'nt' else 'clear')                # limpa a tela do terminal
         atualizarPrevisaoTempoArquivo()
     if tarefa == "abrirPtese":
         folder_path = r"C:\Users\Acer\Dropbox\Leo\3 - Dr-Pesquisa-WaterPhanton3D\0-Tese"
@@ -91,15 +84,7 @@ def perguntarTarefa():
         reniciar()
     tarefasExecutar()
 
-
-
-
-
-
-def reniciar():
-    global tela
-    tela= "Cheia"
-    perguntarTarefa()
+# --------------------------------------------->>> Def com retornos de strings
 
 def stringDataAtual(): 
     dias_da_semana = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom']
@@ -109,20 +94,16 @@ def stringDataAtual():
     return data_atual   
 
 def stringNomeArquivo():
-    caminhoArquivo = r"C:\Users\Acer\OneDrive\Documentos\tarefas" #O resto sera preechido por strings
-    nome_arquivo = caminhoArquivo + " de " + stringDataAtual() + ".txt"    #Ex: C:\Users\Acer\OneDrive\Documentos\tarefas de Dom 23-04-2023.txt
+    nome_arquivo = caminhoArquivoSaida + " de " + stringDataAtual() + ".txt"    #Ex: C:\Users\Acer\OneDrive\Documentos\tarefas de Dom 23-04-2023.txt
     return nome_arquivo
 
-def previsaoTempo(): #Quando chamar esse def o retorno sera a string PrevisaoTempo
-    # Buscar credenciais - informacoes que nao podem estar no codigo principal
-    with open(r'C:\Users\Acer\OneDrive\Documentos\CredenciaisAPI\credenciaisAPI.txt') as f:
+def previsaoTempo():                                                #Quando chamar esse def o retorno sera a string PrevisaoTempo # Previsão do tempo API OpenWeather
+    with open(caminhoCredenciais) as f:                             # Buscar credenciais - informacoes que nao podem estar no codigo principal
         conteudo = f.readlines()
-        cidade = conteudo[1].split(';')[0].strip()  #Ler segunda linha coluna 1 (separacao ";" )
-        api_key = conteudo[1].split(';')[1].strip() #Ler segunda linha coluna 2 (separacao ";" )
-        
+        cidade = conteudo[1].split(';')[0].strip()                  #Ler segunda linha coluna 1 (separacao ";" )
+        api_key = conteudo[1].split(';')[1].strip()                 #Ler segunda linha coluna 2 (separacao ";" )
     if verificarInternet() == 1:
         hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
-        # Previsão do tempo API OpenWeather
         url = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&units=metric'
         response = requests.get(url)
         data = response.json()
@@ -134,57 +115,7 @@ def previsaoTempo(): #Quando chamar esse def o retorno sera a string PrevisaoTem
         PrevisaoTempo='Erro: Nao tempo como verificar a previsao do tempo. Sem conexao com a internet'
     return PrevisaoTempo
 
-
-
-
-
-
-
-def tarefasExecutar():
-    # verifica se o arquivo ja existe, se nao existir, cria um novo
-    if not os.path.exists(stringNomeArquivo()):
-        with open(stringNomeArquivo(), "w") as arquivo:
-            with open(stringNomeArquivo(), "r+") as arquivo:
-                conteudo_arquivo = arquivo.read()
-                arquivo.seek(0, 0) #cursor na primeira linha
-                arquivo.write(f"{previsaoTempo()}\n{conteudo_arquivo}")
-                arquivo.seek(0, 1) #cursor na segunda linha
-                arquivo.write("\n") #Efeito de Enter
-                arquivo.write("ID\tData\tHora\tTarefa\n")
-
-    # adiciona a nova tarefa ao final do arquivo
-    id_tarefa = sum(1 for _ in open(stringNomeArquivo())) -2  # conta o numero de linhas para gerar o ID da nova tarefa
-    hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
-    with open(stringNomeArquivo(), "a") as arquivo:
-        arquivo.write(f"{id_tarefa}\t{stringDataAtual()}\t{hora_atual}\t{tarefa}\n")
-
-    print("     ")
-    print(f"Tarefa registrada com sucesso no arquivo {stringNomeArquivo()}!")
-    print("     ")
-    time.sleep(5)
-
-    perguntarTarefa() # Inicia a funcao 
-
-def atualizarPrevisaoTempoArquivo():
-    with open(stringNomeArquivo(), 'r') as infoArquivo:
-        linhas = infoArquivo.readlines()
-        infoArquivo = ''.join(linhas[3:])
-
-        with open(stringNomeArquivo(), "w") as arquivo:
-            with open(stringNomeArquivo(), "r+") as arquivo:
-                conteudo_arquivo = arquivo.read()
-                arquivo.seek(0, 0) #cursor na primeira linha
-                arquivo.write(f"{previsaoTempo()}\n{conteudo_arquivo}")
-                arquivo.seek(0, 1) #cursor na segunda linha
-                arquivo.write("\n") #Efeito de Enter
-                arquivo.write("ID\tData\tHora\tTarefa\n")
-
-    with open(stringNomeArquivo(), "a") as arquivo:
-        arquivo.write(infoArquivo)
-    perguntarTarefa()
- 
-def verificarInternet():
-    # Verifica conectividade com a internet
+def verificarInternet():                                            # Verifica conectividade com a internet
     try:
         requests.get('https://www.google.com/')
         net=1
@@ -192,28 +123,63 @@ def verificarInternet():
         net=0
     return net
 
+# --------------------------------------------->>> Def que somente executam funcções e depois retornam para o input
+
+def reniciar():
+    global tela
+    tela= "Cheia"
+    perguntarTarefa()
+
+def tarefasExecutar():
+    if not os.path.exists(stringNomeArquivo()):                    # verifica se o arquivo ja existe, se nao existir, cria um novo
+        with open(stringNomeArquivo(), "w") as arquivo:
+            with open(stringNomeArquivo(), "r+") as arquivo:
+                conteudo_arquivo = arquivo.read()
+                arquivo.seek(0, 0)                                 #cursor na primeira linha
+                arquivo.write(f"{previsaoTempo()}\n{conteudo_arquivo}")
+                arquivo.seek(0, 1)                                 #cursor na segunda linha
+                arquivo.write("\n")                                #Efeito de Enter
+                arquivo.write("ID\tData\tHora\tTarefa\n")
+    id_tarefa = sum(1 for _ in open(stringNomeArquivo())) -2       # conta o numero de linhas para gerar o ID da nova tarefa # adiciona a nova tarefa ao final do arquivo
+    hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+    with open(stringNomeArquivo(), "a") as arquivo:
+        arquivo.write(f"{id_tarefa}\t{stringDataAtual()}\t{hora_atual}\t{tarefa}\n")
+    print("     ")
+    print(f"Tarefa registrada com sucesso no arquivo {stringNomeArquivo()}!")
+    print("     ")
+    time.sleep(5)
+    perguntarTarefa()                                              # Inicia a funcao 
+
+def atualizarPrevisaoTempoArquivo():
+    with open(stringNomeArquivo(), 'r') as infoArquivo:
+        linhas = infoArquivo.readlines()
+        infoArquivo = ''.join(linhas[3:])
+        with open(stringNomeArquivo(), "w") as arquivo:
+            with open(stringNomeArquivo(), "r+") as arquivo:
+                conteudo_arquivo = arquivo.read()
+                arquivo.seek(0, 0)                                 #cursor na primeira linha
+                arquivo.write(f"{previsaoTempo()}\n{conteudo_arquivo}")
+                arquivo.seek(0, 1)                                 #cursor na segunda linha
+                arquivo.write("\n")                                #Efeito de Enter
+                arquivo.write("ID\tData\tHora\tTarefa\n")
+    with open(stringNomeArquivo(), "a") as arquivo:
+        arquivo.write(infoArquivo)
+    perguntarTarefa()
+ 
 def openAIconfig():
-    os.system('cls' if os.name == 'nt' else 'clear')  # limpa a tela do terminal
+    os.system('cls' if os.name == 'nt' else 'clear')               # limpa a tela do terminal
     if verificarInternet() == 0:
         print("Erro: IA sem internet! ")
         time.sleep(5)
         reniciar()
     if verificarInternet() == 1:
-        # Defina o modelo GPT que deseja usar
-        model_engine = "text-davinci-002"
+        with open(caminhoCredenciais) as f:
+            conteudo = f.readlines()
+            openai.api_key = conteudo[2].split(';')[1].strip()     #Ler terceira linha coluna 2 (separacao ";" )
+        model_engine = "text-davinci-002"                          # Defina o modelo GPT que deseja usar
         user_input = "Converse comigo somente em português"
-        # Envie uma consulta para o modelo GPT
-        completions = openai.Completion.create(
-            engine=model_engine,
-            prompt=user_input,
-            max_tokens=1,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-        # Imprima a resposta do modelo GPT
+        completions = openai.Completion.create(engine=model_engine, prompt=user_input, max_tokens=1, n=1, stop=None, temperature=0.5, )  # Envie uma consulta para o modelo GPT
         message = completions.choices[0].text.strip()
-        #print(message)
         openAI()
     
 def openAI():
@@ -222,24 +188,16 @@ def openAI():
         time.sleep(5)
         reniciar()
     if verificarInternet() == 1:
-        # Defina o modelo GPT que deseja usar
-        model_engine = "text-davinci-002"
+        with open(caminhoCredenciais) as f:
+            conteudo = f.readlines()
+            openai.api_key = conteudo[2].split(';')[1].strip()       #Ler terceira linha coluna 2 (separacao ";" )
+        model_engine = "text-davinci-002"                            # Defina o modelo GPT que deseja usar
         user_input = input(">>>> ")
-        os.system('cls' if os.name == 'nt' else 'clear')  # limpa a tela do terminal
+        os.system('cls' if os.name == 'nt' else 'clear')             # limpa a tela do terminal
         if user_input == "s" or user_input == "r":
-            reniciar()
-        # Envie uma consulta para o modelo GPT
-        completions = openai.Completion.create(
-            engine=model_engine,
-            prompt=user_input,
-            max_tokens=200,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-
-        # Imprima a resposta do modelo GPT
-        message = completions.choices[0].text.strip()
+            reniciar()      
+        completions = openai.Completion.create( engine=model_engine, prompt=user_input, max_tokens=200, n=1, stop=None, temperature=0.5, )  # Envie uma consulta para o modelo GPT
+        message = completions.choices[0].text.strip()               # Imprima a resposta do modelo GPT
         print(user_input)
         print(" ")
         print(message)
@@ -248,12 +206,7 @@ def openAI():
         time.sleep(5)
         openAI()
 
-
-
-
-
-
-
+# --------------------------------------------->>> Depois de ler todo o codigo o programa executa o primeiro Def e determina uma variavel
 
 tela= "Cheia"
-perguntarTarefa() # Inicia a funcao 
+perguntarTarefa()                                                   # Inicia a funcao 
